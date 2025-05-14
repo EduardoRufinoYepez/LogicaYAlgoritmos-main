005172610078e04ec4cd31773eec8a704709a1ea
EduardoYepez <laloyepez5@gmail.com>
2025-05-13 19:28:10 -0600

    2

--- 4.3.1_DosPunteros/script.js
+++ 4.3.1_DosPunteros/script.js
@@ -1,11 +1,43 @@
-function encontrarParDeSuma(arreglo, sumaObjetivo){
-    for(let i = 0; 1 < arreglo.length; i++){
-        for(let j = i + 1; j < arreglo.length; j++){
-            console.log(`${arreglo[i]} + ${arreglo[j]} = ${arreglo[i] + arreglo[j]}`);
+function encontrarParDeSuma(arreglo, sumaObjetivo) {
+    let indicesSuma = [-1, -1];
+
+    for (let i = 0; i < arreglo.length; i++) {
+        for (let j = i + 1; j < arreglo.length; j++) {
+            //console.log(`${arreglo[i]} + ${arreglo[j]} := ${arreglo[i] + arreglo[j]}`);
+            if (arreglo[i] + arreglo[j] === sumaObjetivo) {
+                //console.log(`La suma de ${arreglo[i]} + ${arreglo[j]} me da el valor buscado`);
+                indicesSuma[0] = i;
+                indicesSuma[1] = j;
+                return indicesSuma;
+            }
         }
     }
+
+    return indicesSuma;
 }
 
-let arreglo = [1, 2, 5, 7, 9, 10, 12 ,13, 15 , 20, 21, 22, 25 ,27, 27];
-let sumaObjetivo = 30;
-encontrarParDeSuma(arreglo, sumaObjetivo);
+function imprimirIndicesYValores(arreglo, indicesSumaResultado, valorObjetivo) {
+    console.log(`Valor objetivo: ${valorObjetivo}`);
+    console.log(`Índices: ${indicesSumaResultado[0]} ${indicesSumaResultado[1]}`);
+    console.log(`Valores: ${arreglo[indicesSumaResultado[0]]} ${arreglo[indicesSumaResultado[1]]}`);
+}
+
+let arreglo = [1, 3, 5, 7, 9, 10, 12, 13, 15, 20, 21, 22, 25, 27, 27];
+let valorObjetivo;
+let indicesSumaResultado;
+
+valorObjetivo = 30;
+indicesSumaResultado = encontrarParDeSuma(arreglo, valorObjetivo);
+imprimirIndicesYValores(arreglo, indicesSumaResultado, valorObjetivo);
+
+valorObjetivo = 28;
+indicesSumaResultado = encontrarParDeSuma(arreglo, valorObjetivo);
+imprimirIndicesYValores(arreglo, indicesSumaResultado, valorObjetivo);
+
+valorObjetivo = 45;
+indicesSumaResultado = encontrarParDeSuma(arreglo, valorObjetivo);
+imprimirIndicesYValores(arreglo, indicesSumaResultado, valorObjetivo);
+
+valorObjetivo = 55;
+indicesSumaResultado = encontrarParDeSuma(arreglo, valorObjetivo);
+imprimirIndicesYValores(arreglo, indicesSumaResultado, valorObjetivo);
